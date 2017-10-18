87604d907ccc9d0e6003b4d716f9cfeaa51b525d
Kel Kanhirun <kel.kanhirun@gmail.com>
2017-10-18 10:02:52 -0500

    Styles the code by removing a describe block

--- Sixpence/ShoppingCartViewControllerSpec.swift
+++ Sixpence/ShoppingCartViewControllerSpec.swift
@@ -5,56 +5,53 @@ import Nimble
 
 final class ShoppingCartViewControllerSpec: QuickSpec {
     override func spec() {
-        
-        describe("\(ShoppingCartViewController.self)") {
-            
-            var subject: ShoppingCartViewController!
 
-            var textInput: TextInputViewModel!
-            var shoppingCart: ShoppingCart!
+        var subject: ShoppingCartViewController!
+
+        var textInput: TextInputViewModel!
+        var shoppingCart: ShoppingCart!
+
+        beforeEach {
+            subject = UIStoryboard(name: "DecisionScreen", bundle: nil)
+                .instantiateViewController(withIdentifier: "ShoppingCartViewController")
+                as! ShoppingCartViewController
+
+            textInput = TextInputViewModel()
+            shoppingCart = ShoppingCart()
+            subject.textInputVM = textInput
+            subject.shoppingCart = shoppingCart
+
+            // TODO: Create a test factory method
+            // This must be called last
+            _ = subject.view
+        }
+
+        it("shows 0 at first") {
+            expect(subject.itemLabel.text) == "-$0.00"
+        }
+
+        context("when user has entered an amount") {
 
             beforeEach {
-                subject = UIStoryboard(name: "DecisionScreen", bundle: nil)
-                    .instantiateViewController(withIdentifier: "ShoppingCartViewController")
-                    as! ShoppingCartViewController
-
-                textInput = TextInputViewModel()
-                shoppingCart = ShoppingCart()
-                subject.textInputVM = textInput
-                subject.shoppingCart = shoppingCart
-
-                // TODO: Create a test factory method
-                // This must be called last
-                _ = subject.view
+                textInput.setKeys(fromMoney: 99.99)
             }
 
-            it("shows 0 at first") {
+            it("shows the amount on the label") {
+                expect(subject.itemLabel.text) == "-$99.99"
+            }
+
+            it("clears the amount when tapped") {
+                subject.userDidTapShoppingCartIcon()
+
                 expect(subject.itemLabel.text) == "-$0.00"
             }
 
-            context("when user has entered an amount") {
+            it("adds an Item to the Shopping Cart") {
+                subject.userDidTapShoppingCartIcon()
 
-                beforeEach {
-                    textInput.setKeys(fromMoney: 99.99)
-                }
-
-                it("shows the amount on the label") {
-                    expect(subject.itemLabel.text) == "-$99.99"
-                }
-
-                it("clears the amount when tapped") {
-                    subject.userDidTapShoppingCartIcon()
-
-                    expect(subject.itemLabel.text) == "-$0.00"
-                }
-
-                it("adds an Item to the Shopping Cart") {
-                    subject.userDidTapShoppingCartIcon()
-
-                    expect(shoppingCart.size.value) == 1
-                }
+                expect(shoppingCart.size.value) == 1
             }
         }
-        
     }
+
 }
